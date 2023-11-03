11a7eb7e2c4d7ac18d6f4d7faede6162ec9d37b9
Markus2734 <143792742+Markus2734@users.noreply.github.com>
2023-11-03 18:47:13 +0900

    Add files via upload

---
+++ 수정된 파일 업로드/1102/payment-inicis/Order.java
@@ -0,0 +1,16 @@
+package com.gamestoreproject.dto;
+
+import lombok.Data;
+
+@Data
+public class Order {
+	private String ocode;
+	private String odate;
+	private String oprice;
+	private String ooriginprice;
+	private String odiscount;
+	private String occode;
+	private String oitem;
+	private String omcode;
+	private String ostate;
+}
