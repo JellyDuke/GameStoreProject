8f4d4a790616c963fb5a70977b606fb3dc61b139
Markus2734 <143792742+Markus2734@users.noreply.github.com>
2023-11-03 21:50:31 +0900

    Add files via upload

---
+++ 수정된 파일 업로드/1103 coupon/Coupon.java
@@ -0,0 +1,87 @@
+package com.gamestoreproject.dto;
+
+import lombok.Data;
+
+@Data
+public class Coupon {
+	private String ccode;
+	private String ctype;
+	private String cregist;
+	private String cpercent;
+	private String cdeadline;
+	private String cused;
+	private String cowner;
+	private String cdate;
+	private String cstack;
+	private String cnumber;
+	private String datecheck;
+	private String typeCheck;
+	private String cgcode;
+	public String getCcode() {
+		return ccode;
+	}
+	public void setCcode(String ccode) {
+		this.ccode = ccode;
+	}
+	public String getCtype() {
+		return ctype;
+	}
+	public void setCtype(String ctype) {
+		this.ctype = ctype;
+	}
+	public String getCregist() {
+		return cregist;
+	}
+	public void setCregist(String cregist) {
+		this.cregist = cregist;
+	}
+	public String getCpercent() {
+		return cpercent;
+	}
+	public void setCpercent(String cpercent) {
+		this.cpercent = cpercent;
+	}
+	public String getCdeadline() {
+		return cdeadline;
+	}
+	public void setCdeadline(String cdeadline) {
+		this.cdeadline = cdeadline;
+	}
+	public String getCused() {
+		return cused;
+	}
+	public void setCused(String cused) {
+		this.cused = cused;
+	}
+	public String getCowner() {
+		return cowner;
+	}
+	public void setCowner(String cowner) {
+		this.cowner = cowner;
+	}
+	public String getCdate() {
+		return cdate;
+	}
+	public void setCdate(String cdate) {
+		this.cdate = cdate;
+	}
+	public String getCstack() {
+		return cstack;
+	}
+	public void setCstack(String cstack) {
+		this.cstack = cstack;
+	}
+	public String getCnumber() {
+		return cnumber;
+	}
+	public void setCnumber(String cnumber) {
+		this.cnumber = cnumber;
+	}
+	public String getDatecheck() {
+		return datecheck;
+	}
+	public void setDatecheck(String datecheck) {
+		this.datecheck = datecheck;
+	}
+	
+}
